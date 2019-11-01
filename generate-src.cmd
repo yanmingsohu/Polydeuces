be7687d3fa3c33c5b0560ff99f2d35310321ae52
yanming <yanming-sohu@sohu.com>
2019-11-01 13:46:38 +0800

    增加 sed 脚本, 以转换 cpp 运行时 g4 文件
    Signed-off-by: yanming <yanming-sohu@sohu.com>

--- generate-src.cmd
+++ generate-src.cmd
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 echo 生成 cpp 解析框架代码
 
@@ -6,10 +6,11 @@ set ANTLR=%cd%\deps/antlr/antlr-4.7.2-complete.jar
 set LIB=%cd%\deps\javascript-grammars\javascript
 set LEXER=%LIB%\JavaScriptLexer.g4
 set PARSER=%LIB%\JavaScriptParser.g4
+
 set OUTDIR=%cd%\generate
 set O_LEXER=%OUTDIR%\JavaScriptLexer.g4
 set O_PARSER=%OUTDIR%\JavaScriptParser.g4
 
 
 java -jar %ANTLR% -Dlanguage=Cpp -listener -visitor -o %OUTDIR% -lib %LIB% -package PolydeucesEngine %O_LEXER% %O_PARSER%
-copy %LIB%/Cpp %OUTDIR%
+rem xcopy %LIB%\Cpp %OUTDIR% /Y
--- pitch-garammars.cmd
+++ pitch-garammars.cmd
@@ -3,10 +3,14 @@
 set LIB=%cd%\deps\javascript-grammars\javascript
 set I_LEXER=%LIB%\JavaScriptLexer.g4
 set I_PARSER=%LIB%\JavaScriptParser.g4
+
 set OUTDIR=%cd%\generate
 set O_LEXER=%OUTDIR%\JavaScriptLexer.g4
 set O_PARSER=%OUTDIR%\JavaScriptParser.g4
+set SED_SCRIPT=%OUTDIR%\pitch.sed
 
 rem 从源代码中的 g4 文件生成 cpp 运行时的最终 g4 文件
-rem diff -p2  %I_LEXER% %O_LEXER%
-rem diff -p2 %I_PARSER% %O_PARSER%
+sed -f %SED_SCRIPT% %I_PARSER% > %O_PARSER%
+sed -f %SED_SCRIPT% %I_LEXER%  > %O_LEXER%
+
+echo ok
